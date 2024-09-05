d282aa0cc00fa0c68f9562fcc5018730bac588ba
Kayshav <kayshav_renganathan@hotmail.com>
2024-09-05 15:44:32 +0800

    add ticket selector automation

--- bot.py
+++ bot.py
@@ -98,6 +98,63 @@ def dateSelector(driver, date, month, sleep=True):
     search_button.click()
     time.sleep(sleeptime)
 
+def ticketSelector(driver, ticket_time, sleep=True):
+    #Add Human Response Time
+    sleeptime = 0
+    if sleep:
+        sleeptime=1
+
+    print("Selecting Ticket...")
+    #Go through Table Rows
+    # Make Python sleep for some time 
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.XPATH, 
+        "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table"))
+    )
+    
+    # Obtain the number of rows in body 
+    rows = 1+len(driver.find_elements(By.XPATH, 
+        "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody/tr")) 
+    
+    # Obtain the number of columns in table 
+    cols = len(driver.find_elements(By.XPATH, 
+        "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody/tr[1]/td")) 
+    
+    # Printing the table headers 
+    # print("Locators           "+"             Description") 
+    
+    # # Printing the data of the table 
+    # for r in range(1, rows): 
+    #     for p in range(1, cols+1): 
+    #         # obtaining the text from each column of the table 
+    #         value = driver.find_element(By.XPATH, 
+    #             "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody/tr["+str(r)+"]/td["+str(p)+"]").text 
+    #         print(value, end='       ') 
+    #     print() 
+    time.sleep(sleeptime)
+    
+    #Select Ticket
+    for r in range(1, rows): 
+        p = 2
+        value = driver.find_element(By.XPATH, 
+            "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody/tr["+str(r)+"]/td["+str(p)+"]").text
+        print("Row:" + str(r) + " Value: " + str(value))
+        #Find ticket time
+        if value==ticket_time:
+            #Select Ticket
+            WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody/tr["+str(r)+"]/td[7]/a"))
+            )
+            select_button = driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody/tr["+str(r)+"]/td[7]/a")
+            select_button.click()
+    time.sleep(sleeptime)
+
+    # WebDriverWait(driver, 30).until(
+    #     EC.presence_of_element_located((By.XPATH, "//tr[contains(@data-hourminute='1630')]/td[contains(@class='text-left')]//a[contains(@class='class='btn select-btn btn-seat-layout')]"))
+    # )
+    # date_number = driver.find_element(By.XPATH, "//tr[contains(@data-hourminute='1630')]/td[contains(@class='text-left')]//a[contains(@class='class='btn select-btn btn-seat-layout')]")
+    # date_number.click()
+
 
 def main():
     service = Service(executable_path="/opt/homebrew/bin/chromedriver")
@@ -109,6 +166,9 @@ def main():
 
     #Select Date
     dateSelector(driver=driver, date=ticket_date, month=month, sleep=False)
+
+    #Select Ticket
+    ticketSelector(driver=driver, ticket_time=ticket_time, sleep=False)
     print("---Done Executing---")
     time.sleep(10)
 
